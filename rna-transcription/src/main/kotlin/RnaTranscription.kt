6cb84924c2acf8231f3ca7a3546b0d5651c4bb3b
Paul Baker <paul.nelson.baker@gmail.com>
2017-04-24 06:57:00 -0600

    Convert if/else-if/else structure to when structure which is a lot prettier

--- rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,17 +1,13 @@
 fun transcribeToRna(dna: String): String {
     val rnaBuilder: StringBuilder = StringBuilder()
 
-    for (letter in dna) {
-        if (letter == 'G') {
-            rnaBuilder.append('C')
-        } else if (letter == 'C') {
-            rnaBuilder.append('G')
-        } else if (letter == 'T') {
-            rnaBuilder.append('A')
-        } else if (letter == 'A') {
-            rnaBuilder.append('U')
-        } else {
-            throw IllegalArgumentException("Illegal DNA string.")
+    for (letter: Char in dna) {
+        when (letter) {
+            'G' -> rnaBuilder.append('C')
+            'C' -> rnaBuilder.append('G')
+            'T' -> rnaBuilder.append('A')
+            'A' -> rnaBuilder.append('U')
+            else -> throw IllegalArgumentException("Illegal DNA string.")
         }
     }
 
