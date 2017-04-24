16c6766ee3c156aa622b58f80c953c0879f0c03c
Paul Baker <paul.nelson.baker@gmail.com>
2017-04-24 06:34:59 -0600

    Split the constructor for readability

--- gigasecond/src/main/kotlin/Gigasecond.kt
+++ gigasecond/src/main/kotlin/Gigasecond.kt
@@ -6,7 +6,8 @@ class Gigasecond {
 
     val date: LocalDateTime
 
-    constructor(birthDate: LocalDate) : this(LocalDateTime.of(birthDate, LocalTime.MIDNIGHT))
+    constructor(birthDate: LocalDate) :
+            this(LocalDateTime.of(birthDate, LocalTime.MIDNIGHT))
 
     constructor(birthDateTime: LocalDateTime) {
         this.date = birthDateTime.plusSeconds(1_000_000_000)
